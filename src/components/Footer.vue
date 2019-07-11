59ea8ff23f14a054a16603ec35491a4efed4c07e
muller <greatpage1@gmail.com>
2019-07-11 16:09:31 +0800

    init

---
+++ src/components/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="footer">
+    <div>网站 | 关于我们</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.footer{
+    display: fixed;
+    bottom: 0;
+    background: #cccccc;
+    width: 100%;
+    height: 100px;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="header">
+    <div>酒业大数据</div>
+    <div>table 导航</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.header{
+    background: #cccccc;
+    width: 100%;
+    height: 200px;
+}
+</style>
